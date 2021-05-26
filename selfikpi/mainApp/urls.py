93578ff1af4e462608d5bfceb85d92d0016a4ec3
Phelippe Souza-Herod <souzaherodp@gmail.com>
2021-05-25 21:29:45 -0400

    add path for sendEmail view

--- selfikpi/mainApp/urls.py
+++ selfikpi/mainApp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import defaultView, showMessages, askQuestion
+from .views import defaultView, showMessages, askQuestion, sendEmail
 
 urlpatterns = [
     path('', askQuestion),
-    path('messages/',showMessages)
+    path('messages/',showMessages),
+    path('sendEmail/<str:email>/<int:user_id>/',sendEmail,name='sendEmail')
 ]
